b0c032ed74f0c9f7d6c9697f1af1196703aae0c0
SolSahn <solsahn@gmail.com>
2018-06-19 14:25:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         
-        <title>Sol's Spectacular Site</title>
+        <title>Sol's Site</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <link rel="stylesheet" type="text/css" href="style.css">
         <script src="script.js"></script>
@@ -9,17 +9,26 @@
     </head>
     <body>
         
-        <?php
-            
-            for ($i = 1; $i <= 50; $i++) {
-                echo $i;
-                echo " ";
-                if (($i % 5) == 0) {
-                    echo "<br>";
-                }
-            }
-            
-        ?>
+    	<form action = "<?php $_PHP_SELF ?>" method = "GET">
+        	Post: <input type="text" name="post"/>
+         	<input type = "submit"/>
+      	</form>
+
+    	<?php
+
+    		$log = fopen('log.txt',"a+") or die("Unable to open post log.");
+    		if (isset($_GET["post"])) {
+    			if ($_GET["post"] != "clearlogs") {
+    				echo "Post shared!<br><br>";
+    				fwrite($log,$_GET["post"]."<br>");
+    			} else {
+    				file_put_contents('log.txt',"");
+    			}
+    		}
+    		echo substr(readfile('log.txt'), 0, -3);
+    		fclose($log);
+
+    	?>
         
     </body>
 </html>
