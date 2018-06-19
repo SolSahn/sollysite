06ecb6eae39e73982fe13c79448a5d8e4ad78e44
SolSahn <solsahn@gmail.com>
2018-06-19 19:49:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
     		$log = fopen('log.txt',"a+") or die("Unable to open post log."); // creates and opens log variable
     		if (isset($_GET["post"])) { // checks if a post is being made
     			if ($_GET["post"] != "clearlogs") {
-    			    if (strlen(preg_replace(' ','',$_GET["post"])) > 0) {
+    			    if (strlen(str_replace(' ','',$_GET["post"])) > 0) {
         				echo "Post shared!<br><br>";
         				fwrite($log,sanitize($_GET["post"])."<br>"); // writes sanitized post to log along with a br
     			    }
