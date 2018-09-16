7c85a734237140ebb1e189a5360b454cb8b07c5a
solsahn <solsahn@gmail.com>
2018-09-16 17:13:03 -0400

    updated canvas position and size

--- index.php
+++ index.php
@@ -1,24 +1,23 @@
 <html>
     <head>
-        
+
         <title>Sol's Site</title>
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <link rel="stylesheet" type="text/css" href="style.css">
         <script src='https://www.google.com/recaptcha/api.js'></script>
         <script src="p5.js"></script>
         <script src="script.js"></script>
-        
+
     </head>
     <body>
-        
-        <div id="sketch-holder" style="position: absolute; left: 500px; top: 10px"></div>
-        
+
     	<form action = "" method = "POST">
         	Post: <input type="text" name="post"/>
         	<div class="g-recaptcha" data-sitekey="6LcBe3AUAAAAAKPj1yH-qjqrjzHxB-kazd5_bpUa"></div>
          	<input type = "submit"/>
       	</form>
-      	
-      	        
-    <div style='text-align: right;position: fixed;z-index:9999999;bottom: 0; width: 100%;cursor: pointer;line-height: 0;display:block !important;'><a title="000webhost logo" rel="nofollow" target="_blank" href="https://www.000webhost.com/?utm_source=000webhostapp&amp;utm_campaign=000_logo&amp;utm_campaign=ss-footer_logo&amp;utm_medium=000_logo&amp;utm_content=website"><img src="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png" alt="000webhost logo"></a></div></body>
+
+      	now with captcha and improved balls!<br>
+    <div id="sketch-holder"></div>
+        
 </html>
--- script.js
+++ script.js
@@ -1,61 +1,51 @@
-window.onload = function() {
-
-quantity = 40;
-gravx = 0;
-gravy = 0.3;
-class Ball {
-  constructor(r,posx,posy,velx,vely,accx,accy) {
-    this.r = r;
-    this.posx = posx;
-    this.posy = posy;
-    this.velx = velx;
-    this.vely = vely;
-    this.accx = accx;
-    this.accy = accy;
-    this.b = document.createElement('div');
-    this.b.style.width = (this.r*2) + 'px';
-    this.b.style.height = (this.r*2) + 'px';
-    this.b.style.position = 'absolute';
-    this.b.style.background = 'rgb('+(Math.random()*180+75)+','+(Math.random()*180+75)+','+(Math.random()*180+75)+')';
-    this.b.style.borderRadius = '50%';
-    document.body.appendChild(this.b);
+var movers;
+var amount;
+var gravity;
+function setup() {
+  let canvas = createCanvas(800, 800);
+  canvas.parent('sketch-holder');
+  noStroke();
+  movers = [];
+  amount = 50;
+  gravity = createVector(0, 0.1);
+  for (let i = 0; i < amount; i++) {
+    movers[i] = new Mover(createVector(width/2, height/2), createVector(random(-4, 4), random(-4, 4)), 20, color(random(255), random(255), random(255)));
   }
-  addForce(fx,fy) {
-    this.accx += fx;
-    this.accy += fy;
+}
+function draw() {
+  background(0);
+  for (let i = 0; i < amount; i++) {
+    movers[i].addForce(gravity);
+    movers[i].update();
+    movers[i].display();
+  }
+}
+class Mover {
+  constructor(pos, vel, rad, col) {
+    this.pos = pos;
+    this.vel = vel;
+    this.acc = createVector(0, 0);
+    this.rad = rad;
+    this.col = col;
+  }
+  addForce(force) {
+    this.acc.add(force);
   }
   update() {
-    this.posx += this.velx;
-    this.posy += this.vely;
-    this.velx += this.accx;
-    this.vely += this.accy;
-    this.accx = 0;
-    this.accy = 0;
-    this.addForce(gravx,gravy);
-    if (this.posx - this.r <= 250) {
-      this.velx = Math.abs(this.velx)*0.983;
-    } else if (this.posx + this.r >= 1100) {
-		  this.velx = -Math.abs(this.velx)*0.983;
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    if (this.pos.x <= this.rad) {
+      this.vel.x = Math.abs(this.vel.x);
+    } else if (this.pos.x >= width - this.rad) {
+      this.vel.x = -Math.abs(this.vel.x);
     }
-    if (this.posy + this.r >= 550) {
-      this.vely *= -0.983;
+    if (this.pos.y >= height - this.rad) {
+      this.vel.y = -Math.abs(this.vel.y);
     }
   }
-  draw() {
-    this.b.style.left = this.posx + 'px';
-    this.b.style.top = this.posy + 'px';
+  display() {
+    fill(this.col);
+    ellipse(this.pos.x, this.pos.y, this.rad*2, this.rad*2);
   }
-}
-var balls = {};
-for (i=0;i<quantity;i++) {
-  balls[i] = new Ball(20,Math.random()*200+500,Math.random()*50+100,Math.random()*20-10,Math.random()*10-5,0,0);
-}
-function updateBalls() {
-  for (var i in balls) {
-    balls[i].update();
-    balls[i].draw();
-  }
-}
-setInterval(updateBalls,30);
-
 }
